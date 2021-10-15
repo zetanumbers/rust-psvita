d9f13635a14aa16b5323a0ddfc7f7dcc1b3c141a
ZetaNumbers <zeta0.114942@gmail.com>
2021-10-15 17:06:40 +0300

    Small code changes

--- psvita_sce_types/src/lib.rs
+++ psvita_sce_types/src/lib.rs
@@ -1,10 +1,13 @@
-use object::endian::LittleEndian;
+use object::{
+    endian::{self, LittleEndian},
+    pod,
+};
 use sha1::{Digest, Sha1};
 
-pub type Nid = object::endian::U32<LittleEndian>;
+pub type Nid = endian::U32<LittleEndian>;
 
 pub fn generate_nid(name: &[u8]) -> Nid {
     let digest: [u8; 20] = Sha1::digest(name).into();
-    let (nid, _tail) = object::pod::from_bytes(&digest[..4]).unwrap();
+    let (nid, _tail) = pod::from_bytes(&digest).unwrap();
     *nid
 }
