bc2680c5125b9c65d89cd5f09df2782a242bb9fe
ZetaNumbers <zeta0.114942@gmail.com>
2021-10-17 19:36:18 +0300

    Switch to `endian_codec` and `bytemuck`

--- psvita_sce_types/src/lib.rs
+++ psvita_sce_types/src/lib.rs
@@ -1,16 +1,15 @@
-use object::{endian, pod};
+use bytemuck::{Pod, Zeroable};
+use core::convert::TryInto;
+use endian_codec::{DecodeLE, EncodeLE, PackedSize};
 use sha1::{Digest, Sha1};
 
-pub type Endian = endian::LittleEndian;
-pub type U16 = endian::U16Bytes<Endian>;
-pub type U32 = endian::U32Bytes<Endian>;
-pub type USize = U32;
-pub type Nid = U32;
+pub type Nid = u32;
+pub type Address = u32;
 
 pub fn generate_nid(name: &[u8]) -> Nid {
     let digest: [u8; 20] = Sha1::digest(name).into();
-    let (nid, _tail) = pod::from_bytes(&digest).unwrap();
-    *nid
+    let digest_tail: &[u8; 4] = digest[..4].try_into().unwrap();
+    Nid::from_le_bytes(*digest_tail)
 }
 
 pub mod sce_module_info {
@@ -20,10 +19,10 @@ pub mod sce_module_info {
 
     /// Common beginning of `SceModuleInfo` structs.
     #[repr(C)]
-    #[derive(Copy, Clone, Debug)]
+    #[derive(Copy, Clone, Debug, Zeroable, Pod, PackedSize, EncodeLE, DecodeLE)]
     pub struct Common {
         /// Attributes of the module
-        pub attributes: U16,
+        pub attributes: u16,
         /// Major version of the module (usually set to 1) followed by Minor version of the module (usually set to 1)
         pub module_version: [u8; 2],
         /// Name of the module. Null-terminated string.
@@ -33,45 +32,45 @@ pub mod sce_module_info {
     }
 
     #[repr(C)]
-    #[derive(Copy, Clone, Debug)]
+    #[derive(Copy, Clone, Debug, Zeroable, Pod, PackedSize, EncodeLE, DecodeLE)]
     pub struct V6 {
         pub common: Common,
         /// Global pointer value for MIPS, TOC address (address of .toc) for PowerPC, always 0 for ARM
-        pub gp_value: USize,
+        pub gp_value: Address,
         /// Offset to top of exports array
-        pub export_top: USize,
+        pub export_top: Address,
         /// Offset to bottom of exports array
-        pub export_bottom: USize,
+        pub export_bottom: Address,
         /// Offset to top of imports array
-        pub import_top: USize,
+        pub import_top: Address,
         /// Offset to bottom of imports array
-        pub import_bottom: USize,
+        pub import_bottom: Address,
         /// It was wrongly named module NID. It is a sort of hash to ensure integrity and versioning.
-        pub debug_fingerprint: U32,
+        pub debug_fingerprint: u32,
         /// Offset to start of TLS (Thread Local Storage)
-        pub tls_start: USize,
+        pub tls_start: Address,
         /// Certainly equals (tls_end - tls_start)
-        pub tls_filesz: USize,
+        pub tls_filesz: Address,
         /// Certainly equals (tls_initialized_data_end - tls_start)
-        pub tls_memsz: USize,
+        pub tls_memsz: Address,
         /// Offset to module_start function. To disable set it to:
         /// - `-1` according to [Henkaku wiki](https://wiki.henkaku.xyz/vita/Modules#SceModuleInfo)
         /// - `0` according to [vitasdk toolchain](https://github.com/vitasdk/vita-toolchain/blob/a075d3ab2963d6b12e1a51b6816022d4f0d2c41d/src/sce-elf-defs.h#L36)
         /// TODO: test both
-        pub start_entry: USize,
+        pub start_entry: Address,
         /// Offset to module_stop function. To disable set it to:
         /// - `-1` according to [Henkaku wiki](https://wiki.henkaku.xyz/vita/Modules#SceModuleInfo)
         /// - `0` according to [vitasdk toolchain](https://github.com/vitasdk/vita-toolchain/blob/a075d3ab2963d6b12e1a51b6816022d4f0d2c41d/src/sce-elf-defs.h#L37)
         /// TODO: test both
-        pub stop_entry: USize,
+        pub stop_entry: Address,
         /// Offset to top of ARM EXIDX (optional)
-        pub arm_exidx_top: USize,
+        pub arm_exidx_top: Address,
         /// Offset to bottom of ARM EXIDX (optional)
-        pub arm_exidx_bottom: USize,
+        pub arm_exidx_bottom: Address,
         /// Offset to top of ARM EXTAB (optional)
-        pub arm_extab_top: USize,
+        pub arm_extab_top: Address,
         /// Offset to bottom of ARM EXTAB (optional)
-        pub arm_extab_bottom: USize,
+        pub arm_extab_bottom: Address,
     }
 
     #[cfg(test)]
@@ -81,7 +80,4 @@ pub mod sce_module_info {
         assert_eq!(size_of::<Common>(), 0x20);
         assert_eq!(size_of::<V6>(), 0x5C);
     }
-
-    unsafe impl pod::Pod for Common {}
-    unsafe impl pod::Pod for V6 {}
 }
--- psvita_sce_types/tests/tests.rs
+++ psvita_sce_types/tests/tests.rs
@@ -2,6 +2,6 @@
 fn henkaku_nid_example() {
     assert_eq!(
         0xEEDA2E54,
-        psvita_sce_types::generate_nid(b"sceDisplayGetFrameBuf").get(<_>::default())
+        psvita_sce_types::generate_nid(b"sceDisplayGetFrameBuf")
     );
 }
