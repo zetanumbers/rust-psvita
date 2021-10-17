2eec4d174e343073f98b4551452dfd4f0010366d
ZetaNumbers <zeta0.114942@gmail.com>
2021-10-17 20:40:36 +0300

    Add different SceModuleInfo versions Remove endian_codec

--- psvita_sce_types/src/lib.rs
+++ psvita_sce_types/src/lib.rs
@@ -1,6 +1,5 @@
 use bytemuck::{Pod, Zeroable};
 use core::convert::TryInto;
-use endian_codec::{DecodeLE, EncodeLE, PackedSize};
 use sha1::{Digest, Sha1};
 
 pub type Nid = u32;
@@ -19,7 +18,7 @@ pub mod sce_module_info {
 
     /// Common beginning of `SceModuleInfo` structs.
     #[repr(C)]
-    #[derive(Copy, Clone, Debug, Zeroable, Pod, PackedSize, EncodeLE, DecodeLE)]
+    #[derive(Copy, Clone, Debug, Zeroable, Pod)]
     pub struct Common {
         /// Attributes of the module
         pub attributes: u16,
@@ -32,27 +31,40 @@ pub mod sce_module_info {
     }
 
     #[repr(C)]
-    #[derive(Copy, Clone, Debug, Zeroable, Pod, PackedSize, EncodeLE, DecodeLE)]
-    pub struct V6 {
-        pub common: Common,
-        /// Global pointer value for MIPS, TOC address (address of .toc) for PowerPC, always 0 for ARM
-        pub gp_value: Address,
-        /// Offset to top of exports array
-        pub export_top: Address,
-        /// Offset to bottom of exports array
-        pub export_bottom: Address,
-        /// Offset to top of imports array
-        pub import_top: Address,
-        /// Offset to bottom of imports array
-        pub import_bottom: Address,
-        /// It was wrongly named module NID. It is a sort of hash to ensure integrity and versioning.
-        pub debug_fingerprint: u32,
-        /// Offset to start of TLS (Thread Local Storage)
-        pub tls_start: Address,
-        /// Certainly equals (tls_end - tls_start)
-        pub tls_filesz: Address,
-        /// Certainly equals (tls_initialized_data_end - tls_start)
-        pub tls_memsz: Address,
+    #[derive(Copy, Clone, Debug, Zeroable, Pod)]
+    pub struct AddressRange {
+        /// Offset to top of address range
+        top: Address,
+        /// Offset to bottom of address range
+        bottom: Address,
+    }
+
+    #[repr(C)]
+    #[derive(Copy, Clone, Debug, Zeroable, Pod)]
+    pub struct PublicApi {
+        /// Exports array
+        exports: AddressRange,
+        /// Imports array
+        imports: AddressRange,
+    }
+
+    /// Global pointer value for MIPS, TOC address (address of .toc) for PowerPC, always 0 for ARM
+    #[repr(transparent)]
+    #[derive(Copy, Clone, Debug, Zeroable, Pod)]
+    pub struct GPValue {
+        pub inner: Address,
+    }
+
+    /// It was wrongly named module NID. It is a sort of hash to ensure integrity and versioning.
+    #[repr(transparent)]
+    #[derive(Copy, Clone, Debug, Zeroable, Pod)]
+    pub struct DebugFingerprint {
+        pub inner: u32,
+    }
+
+    #[repr(C)]
+    #[derive(Copy, Clone, Debug, Zeroable, Pod)]
+    pub struct Entries {
         /// Offset to module_start function. To disable set it to:
         /// - `-1` according to [Henkaku wiki](https://wiki.henkaku.xyz/vita/Modules#SceModuleInfo)
         /// - `0` according to [vitasdk toolchain](https://github.com/vitasdk/vita-toolchain/blob/a075d3ab2963d6b12e1a51b6816022d4f0d2c41d/src/sce-elf-defs.h#L36)
@@ -63,14 +75,85 @@ pub mod sce_module_info {
         /// - `0` according to [vitasdk toolchain](https://github.com/vitasdk/vita-toolchain/blob/a075d3ab2963d6b12e1a51b6816022d4f0d2c41d/src/sce-elf-defs.h#L37)
         /// TODO: test both
         pub stop_entry: Address,
-        /// Offset to top of ARM EXIDX (optional)
-        pub arm_exidx_top: Address,
-        /// Offset to bottom of ARM EXIDX (optional)
-        pub arm_exidx_bottom: Address,
-        /// Offset to top of ARM EXTAB (optional)
-        pub arm_extab_top: Address,
-        /// Offset to bottom of ARM EXTAB (optional)
-        pub arm_extab_bottom: Address,
+    }
+
+    #[repr(C)]
+    #[derive(Copy, Clone, Debug, Zeroable, Pod)]
+    pub struct TlsInfo {
+        /// Offset to start of TLS (Thread Local Storage)
+        pub tls_start: Address,
+        /// Certainly equals (tls_end - tls_start)
+        pub tls_filesz: Address,
+        /// Certainly equals (tls_initialized_data_end - tls_start)
+        pub tls_memsz: Address,
+    }
+
+    /// Address range of ARM EXIDX (optional)
+    #[repr(transparent)]
+    #[derive(Copy, Clone, Debug, Zeroable, Pod)]
+    pub struct ArmExidx {
+        pub inner: AddressRange,
+    }
+
+    /// Address range of ARM EXTAB (optional)
+    #[repr(transparent)]
+    #[derive(Copy, Clone, Debug, Zeroable, Pod)]
+    pub struct ArmExtab {
+        pub inner: AddressRange,
+    }
+
+    #[repr(C)]
+    #[derive(Copy, Clone, Debug, Zeroable, Pod)]
+    pub struct V0 {
+        pub common: Common,
+        pub gp_value: GPValue,
+        pub public_api: PublicApi,
+    }
+
+    #[repr(C)]
+    #[derive(Copy, Clone, Debug, Zeroable, Pod)]
+    pub struct V1 {
+        pub common: Common,
+        pub gp_value: GPValue,
+        pub public_api: PublicApi,
+        pub debug_fingerprint: DebugFingerprint,
+        pub entries: Entries,
+    }
+
+    #[repr(C)]
+    #[derive(Copy, Clone, Debug, Zeroable, Pod)]
+    pub struct V2 {
+        pub common: Common,
+        pub gp_value: GPValue,
+        pub public_api: PublicApi,
+        pub debug_fingerprint: DebugFingerprint,
+        pub entries: Entries,
+        pub arm_exidx: ArmExidx,
+    }
+
+    #[repr(C)]
+    #[derive(Copy, Clone, Debug, Zeroable, Pod)]
+    pub struct V3 {
+        pub common: Common,
+        pub gp_value: GPValue,
+        pub public_api: PublicApi,
+        pub debug_fingerprint: DebugFingerprint,
+        pub entries: Entries,
+        pub arm_exidx: ArmExidx,
+        pub tls: TlsInfo,
+    }
+
+    #[repr(C)]
+    #[derive(Copy, Clone, Debug, Zeroable, Pod)]
+    pub struct V6 {
+        pub common: Common,
+        pub gp_value: GPValue,
+        pub public_api: PublicApi,
+        pub debug_fingerprint: DebugFingerprint,
+        pub tls: TlsInfo,
+        pub entries: Entries,
+        pub arm_exidx: ArmExidx,
+        pub arm_extab: ArmExtab,
     }
 
     #[cfg(test)]
@@ -78,6 +161,10 @@ pub mod sce_module_info {
     fn type_assertions() {
         use core::mem::size_of;
         assert_eq!(size_of::<Common>(), 0x20);
+        assert_eq!(size_of::<V0>(), 0x34);
+        assert_eq!(size_of::<V1>(), 0x40);
+        assert_eq!(size_of::<V2>(), 0x48);
+        assert_eq!(size_of::<V3>(), 0x54);
         assert_eq!(size_of::<V6>(), 0x5C);
     }
 }
