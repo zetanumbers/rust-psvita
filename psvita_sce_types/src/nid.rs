741bd1e3d55c67ca3814a76af14498085bdeefac
ZetaNumbers <zeta0.114942@gmail.com>
2021-10-17 23:07:10 +0300

    Add `nid-generation` feature Reorganize `psvita_sce_types::nid`

--- psvita_sce_types/src/nid.rs
+++ psvita_sce_types/src/nid.rs
@@ -1,21 +1,27 @@
 use bytemuck::{Pod, Zeroable};
-use core::convert::TryInto;
-use sha1::{Digest, Sha1};
 
 #[repr(C)]
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Zeroable, Pod)]
 pub struct Nid(pub u32);
 
-impl From<&[u8]> for Nid {
-    fn from(name: &[u8]) -> Nid {
+#[cfg(feature = "nid-generation")]
+impl Nid {
+    pub fn from_bytes(name: &[u8]) -> Nid {
+        use core::convert::TryInto;
+        use sha1::{Digest, Sha1};
+
         let digest: [u8; 20] = Sha1::digest(name).into();
         let digest_tail: &[u8; 4] = digest[..4].try_into().unwrap();
         Nid(u32::from_le_bytes(*digest_tail))
     }
 }
 
-impl Nid {
-    pub fn from_bytes(name: &[u8]) -> Nid {
-        Nid::from(name)
+#[cfg(test)]
+mod tests {
+    #[cfg(feature = "nid-generation")]
+    #[test]
+    fn henkaku_nid_example() {
+        use super::Nid;
+        assert_eq!(Nid(0xEEDA2E54), Nid::from_bytes(b"sceDisplayGetFrameBuf"));
     }
 }
--- psvita_sce_types/tests/tests.rs
+++
@@ -1,6 +0,0 @@
-use psvita_sce_types::nid::Nid;
-
-#[test]
-fn henkaku_nid_example() {
-    assert_eq!(Nid(0xEEDA2E54), Nid::from_bytes(b"sceDisplayGetFrameBuf"));
-}
